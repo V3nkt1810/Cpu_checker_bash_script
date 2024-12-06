70414b392c65b0964dfa88546d73bfb0fab22bb1
v3nkt1810 <venkatsv1252@gmail.com>
2024-12-06 20:31:50 +0530

    modified:   cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -3,6 +3,7 @@
 if [ -z "$1" ]; then
     echo "Usage: $0 vuser.txt"
     exit 1
+
 fi
  
 server_list=$1
@@ -11,29 +12,27 @@ if [ ! -f "$server_list" ]; then
     echo "File $server_list not found!"
     exit 1
 fi
-
-
+ 
 print_table() {
     printf "%-10s %-20s %-10s %-8s %-s\n" "PID" "USER" "CPU(%)" "COMMAND"
     printf "%-10s %-20s %-10s %-8s %-s\n" "--------" "--------------------" "--------" "-------------------------"
 }
-
-while IFS= read -r server_name; do
+ 
+for server_name in $(cat "$server_list"); do
+    echo "\n---------------------------------------------"
+    echo "Checking CPU utilization on $server_name server"
     echo "---------------------------------------------"
-    echo "Checking CPU utilization on $server_name"
-    echo "---------------------------------------------"
     
-    ssh -q $server_name '
-    print_table() {
-        printf "%-10s %-20s %-10s %-8s %-s\n" "PID" "USER" "CPU(%)" "COMMAND"
-        printf "%-10s %-20s %-10s %-8s %-s\n" "--------" "--------------------" "--------" "-------------------------"
-    }
-    
-    print_table
-    
-    ps -eo pid,user,%cpu,comm --sort=-%cpu | head -6 | tail -5 | while read pid user cpu cmd; do
-        printf "%-10s %-20s %-10s %-8s %-s\n" "$pid" "$user" "$cpu" "$cmd"
-    done
+    ssh -q "$server_name" '
+        print_table() {
+            printf "%-10s %-20s %-10s %-8s %-s\n" "PID" "USER" "CPU(%)" "COMMAND"
+            printf "%-10s %-20s %-10s %-8s %-s\n" "--------" "--------------------" "--------" "-------------------------"
+        }
+        print_table
+        ps -eo pid,user,%cpu,comm --sort=-%cpu | head -6 | tail -5 | while read pid user cpu cmd; do
+            printf "%-10s %-20s %-10s %-8s %-s\n" "$pid" "$user" "$cpu" "$cmd"
+        done
     '
-    echo End "\n"
-done < "$server_list"
+    
+    echo  "\nPlease find the above Top 5 Utilization on $server_name\n"
+done
